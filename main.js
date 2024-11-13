35cdcc76e784a4fdd3b2340fe30adde1d211afc0
vinhfang <gonquy2005@gmail.com>
2024-11-13 21:34:12 +0700

    init file

---
+++ main.js
@@ -0,0 +1,44 @@
+// Thay đổi nội dung búc thư ở đây
+var letterContent =" Chúc mừng sinh nhật em bé iu của cún. Giờ hơn tui 1 tuổi rùi, lại phải lớn lên rùiiii ❤. Anh chúc em tuổi mới có nhiều trải nghiệm thật mới cùng anhhhh, tuổi 20 rực rỡ nha bây biiiii 💌"
+
+// Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
+durationWrite = 50 
+
+// Hiệu ứng gõ chữ
+
+function effectWrite () {
+    var boxLetter = document.querySelector(".letterContent")
+    letterContentSplited = letterContent.split("")
+    
+    letterContentSplited.forEach((val, index) => {
+        setTimeout(() => {
+            boxLetter.innerHTML += val    
+        }, durationWrite* index)
+    })
+}
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.querySelector(".container").classList.add("active")
+    }, 500)
+})
+
+var openBtn = document.querySelector(".openBtn")
+openBtn.addEventListener("click", () => {
+    document.querySelector(".cardValentine").classList.add("active")
+    document.querySelector(".container").classList.add("close")
+})
+
+var cardValentine = document.querySelector(".cardValentine")
+
+cardValentine.addEventListener("click", () => {
+    cardValentine.classList.toggle("open")
+
+    if(cardValentine.className.indexOf("open") != -1) {
+        setTimeout(effectWrite, 500)
+    } else {
+        setTimeout(() => {
+            document.querySelector(".letterContent").innerHTML = ""
+        }, 1000)
+    }
+})
